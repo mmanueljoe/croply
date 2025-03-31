34d211c0ffe4540c52a6d3b459bf6ab60d7999e0
Dasonchris <dasonchris47@gmail.com>
2025-03-31 18:38:53 +0100

    added to trials

--- src/pages/trials.jsx
+++ src/pages/trials.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import "../pages/trials.css";
-import agrow from "../assets/images/agrow.png";  // Correct way to import images
+import superImage from "../assets/images/super.png";
+import last from "../assets/images/last.png";
+import agrow from "../assets/images/agrow.png";   
+
 
 
 const aiLogo = new URL('../assets/images/ai-logo.png', import.meta.url).href;
@@ -17,7 +20,7 @@ export const Trials = () => {
           <div className="search-bar">
             <form action="">
               <div className="search-container">
-                <input type="text" placeholder="Search Agro Inputs..." />
+                <input type="text" placeholder="" />
                 <span className="search-icon">🔍</span>
               </div>
             </form>
@@ -36,34 +39,67 @@ export const Trials = () => {
           <div className="texts">
             <h1 className='sub-head'>D.I AGROW</h1>
             <p className='content'>
-              D.I Grow is a liquid foliar fertilizer designed to enhance plant growth and improve soil quality.
-              It contains a blend of marcronutrients (such as nitrogen, phosphorus, potassium, and magnesium), micronutrients (including iron, maganesse, copper, zinc, boron, and molybdenum), and humic acid.
-              This combination supports various crops, including vegetables, fruits, and flowers. cedargro.com
+              D.I Grow is a liquid foliar fertilizer designed to enhance plant <br/> growth and improve soil quality. <br/>
+              It contains a blend of <br /> marcronutrients (such as nitrogen, phosphorus, potassium, and <br /> magnesium), micronutrients (including iron, maganesse, copper, <br /> zinc, boron, and molybdenum), and humic acid. <br />
+              This combination supports various crops, including vegetables, <br /> fruits, and flowers. <span className='com'>cedargro.com</span> <br />
+              <button className='read'>Read more</button>
             </p>
           </div>
         </div>
       </section>
-{/* 
-      <section className="testimonials">
-        <h2>What Our Users Say</h2>
-        <div className="testimonial">
-          <p>"CroplyAI helped me optimize my farming practices!"</p>
-          <span>- John Doe, Farmer</span>
-        </div>
-        <div className="testimonial">
-          <p>"The insights provided by Agro Inputs are invaluable."</p>
-          <span>- Jane Smith, Agronomist</span>
+      {/* SECOND CARD */}
+       <section className="more-info">
+        <div className="card">
+            <img src={superImage} alt="" />
+          <div className="texts">
+            <h1 className='sub-head'>SUPER GRO</h1>
+            <p className='content'>
+              Super Gro is an organic liquid fertilizer formulated from poultry <br /> droppings and seabird guano. it's designed to enhance soil <br />
+              fertility and promote healthy plant growth. Super Gro acts as a <br /> surfacant, reducing the surface tension of the water,which allows <br />
+              for better water penetration and nutrient absorption by plants. <br /> <span className='com'>agric4profits.com</span> <br />
+              <button className='read'>Read more</button>
+            </p>
+          </div>
         </div>
       </section>
 
-      <section className="contact">
-        <h2>Contact Us</h2>
-        <form>
-          <input type="email" placeholder="Your Email" />
-          <textarea placeholder="Your Message"></textarea>
-          <button type="submit">Send</button>
-        </form>
-      </section> */}
+    {/* Third CArd */}
+    <section className="more-info">
+        <div className="card">
+            <img src={agrow} alt="" />
+          <div className="texts">
+            <h1 className='sub-head'>ALGIFOL</h1>
+            <p className='content'>
+            Algifol is an organic biostimultant derived from brown algae. <br />
+            Through a special process, it concentrates trace elements, <br />
+            vitamins, enzymes, amino acid, and plant hormones that <br />
+            promote natural growth regulation. Algifol is suitable for a <br />
+            wide range of plants, including soft fruits, fruit trees, vegetables, <br />
+            flowers, and grains. <span className='com'>greenfertilizersghana.com</span> <br />
+              <button className='read'>Read more</button>
+            </p>
+          </div>
+        </div>
+      </section>
+
+
+      {/* Fourth Card */}
+      <section className="more-info">
+        <div className="card">
+            <img src={last} alt=""  className='last'/>
+          <div className="texts">
+            <h1 className='sub-head'>PROTIFERT</h1>
+            <p className='content'>
+            ProtiFert is a certified organic liquid fertilizer designed to <br />
+            boost plant vitality. it's suitable for a variety of crops, including <br />
+            vegetables, fruits, and ornamentals. ProtiFert enhances soil <br />
+            fertility and promotes healthy plant growth. <br /> greenfertilizersghana.com <br />
+
+              <button className='read'>Read more</button>
+            </p>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
